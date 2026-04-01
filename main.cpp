c040f6293ed99fc01ab14dad87ec41c5674ee807
lamoirex <ilya-burmistrov-2007@mail.ru>
2026-04-01 13:07:52 +0300

    Window, sprite, movement added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
-int main() {
+#include <SFML/Graphics.hpp>
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode({1080,1080}), "CPPHead");
+    window.setView(sf::View{ {}, static_cast<sf::Vector2f>(window.getSize())});
+
+    sf::CircleShape shape{50};
+    shape.setOrigin({shape.getRadius(),shape.getRadius()});
+
+    sf::Texture texture("Owlet_Monster.png");
+    sf::Sprite sprite{texture};
+    sprite.setScale({3,2});
+    //sprite.setOrigin(sf::Vector2f{sprite.getScale().x * texture.getSize().x,
+                                  // sprite.getScale().y * texture.getSize().y} / 2.f);
+
+    sf::Clock clock{};
+    float speed = 200;
+
+    bool isRunning = true;
+    while (isRunning) {
+        auto elapsed = clock.restart();
+        float dt = elapsed.asSeconds();
+
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {isRunning = false;}
+            if (event->is<sf::Event::Resized>()) {window.setView(sf::View{ {}, static_cast<sf::Vector2f>(window.getSize())});}
+        }
+
+
+        sf::Vector2f movement{};
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {movement.x += 1;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {movement.x -= 1;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {movement.y -= 1;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {movement.y += 1;}
+
+        sprite.setPosition(sprite.getPosition()+movement * speed * dt );
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
     return 0;
 }
